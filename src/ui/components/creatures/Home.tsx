b46a2bf667566375c7ba0511fb34e43b45e9c21f
thisismrali <ali.abdolyousefi@gmail.com>
2024-12-22 20:04:42 +0330

    fix

--- src/ui/components/creatures/Home.tsx
+++ src/ui/components/creatures/Home.tsx
@@ -9,7 +9,7 @@ import TransitionInput from "../molecuels/TransitionInput";
 const Home = () => {
   const [layers] = useAtom(layersAtom);
   return (
-    <div className="w-full h-full flex flex-col items-center ">
+    <div className="w-full h-full flex flex-col items-center space-y-4 ">
       <ViewerBox />
       {layers.map((layer, index) => (
         <VideoBox key={index} item={layer} />
